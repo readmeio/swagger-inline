743354e9907435cf24d7fdacb5f87f4a9b23c3c9
Harold Treen <haroldtreen@gmail.com>
2016-08-14 15:48:32 -0700

    Swagger inline tests + basic usage.

--- src/swagger-inline.js
+++ src/swagger-inline.js
@@ -1,5 +1,24 @@
-function swaggerInline() {
+const glob = require('glob');
+const Promise = require('bluebird');
 
+const Loader = require('./loader');
+
+function swaggerInline(globPattern, options) {
+    if (typeof globPattern !== 'string') {
+        throw new Error('No files specificied...');
+    }
+
+    return new Promise((resolve, reject) => {
+        glob(globPattern, (err, files) => {
+            if (err) {
+                reject(err);
+            }
+
+            Loader.loadFiles(files).then((filesData) => {
+                resolve(filesData.join('\n'));
+            });
+        });
+    });
 }
 
 module.exports = swaggerInline;
--- tests/swagger-inline-test.js
+++ tests/swagger-inline-test.js
@@ -2,7 +2,21 @@ const assert = require('chai').assert;
 const swaggerInline = require('../src/swagger-inline');
 
 describe('Swagger Inline', () => {
-    it('is a function', () => {
-        assert.isFunction(swaggerInline);
+    it('requires inputs', () => {
+        assert.throws(swaggerInline.bind(null, undefined), 'files');
+    });
+
+    it('returns a promise', () => {
+        const returned = swaggerInline('*.js', {});
+
+        assert.isFunction(returned.then);
+        assert.isFunction(returned.catch);
+    });
+
+    it('resolves to a string', (done) => {
+        swaggerInline('./tests/*.js').then((generatedSwagger) => {
+            assert.isString(generatedSwagger);
+            done();
+        }).catch(done);
     });
 });

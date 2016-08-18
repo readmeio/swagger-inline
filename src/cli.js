6eeef74a635e39c4bdf94b947622aeb979b7bebc
Harold Treen <haroldtreen@gmail.com>
2016-08-18 15:17:55 -0700

    Add cli functionality

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,40 @@
+const fs = require('fs');
+const program = require('commander');
+const packageJson = require('../package.json');
+const Options = require('./options');
+const swaggerInline = require('./swagger-inline');
+
 function Cli(args) {
-    console.log(args);
+    program
+        .version(packageJson.version)
+        .usage('[options] <inputGlob>')
+        .option('-b, --base [path]', 'A base swagger file.')
+        .option('-f, --format', 'Output swagger format (.json or .yaml).');
+
+    program.on('--help', () => {
+        [
+            'Example:',
+            '\tswagger-inline "./*.js" --base ./swaggerBase.json',
+        ].forEach((line) => console.log(line));
+    });
+
+    program.parse(args);
+
+    if (program.args.length <= 0) {
+        program.outputHelp();
+        process.exit();
+    }
+
+    const providedOptions = {
+        base: program.base,
+        format: program.format,
+    };
+
+    swaggerInline(program.args[0], providedOptions).then((output) => {
+        const options = new Options(providedOptions);
+
+        fs.writeFileSync(`./swagger${options.getFormat()}`, output, 'utf-8');
+    });
 }
 
 module.exports = Cli;

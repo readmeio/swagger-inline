84386072cdbbe84efc81c7131d5d166daa21037c
Harold Treen <haroldtreen@gmail.com>
2016-08-19 11:54:53 -0700

    Loader returns rejections + can resolve paths.

--- src/loader.js
+++ src/loader.js
@@ -2,22 +2,64 @@ const fs = require('fs');
 const path = require('path');
 const Promise = require('bluebird');
 const jsYaml = require('js-yaml');
+const glob = require('glob');
 
 class Loader {
+    static resolvePaths(filepaths) {
+        let paths = filepaths;
+        if (typeof paths === 'string') {
+            paths = [filepaths];
+        }
+
+        return new Promise((resolve, reject) => {
+            const resolvePromises = paths.map((filepath) => {
+                return Loader.resolvePath(filepath);
+            });
+            Promise.all(resolvePromises).then((resolvePathResults) => {
+                const resolvedPaths = resolvePathResults.reduce((prev, current) => {
+                    return prev.concat(current);
+                }, []);
+                resolve(resolvedPaths);
+            }).catch(reject);
+        });
+    }
+
+    static resolvePath(filepath) {
+        return new Promise((resolve, reject) => {
+            fs.stat(filepath, (err) => {
+                if (err) {
+                    glob(filepath, (globErr, filepaths) => {
+                        if (globErr) {
+                            reject(globErr);
+                        } else {
+                            resolve(filepaths);
+                        }
+                    });
+                } else {
+                    resolve([filepath]);
+                }
+            });
+        });
+    }
+
     static loadFiles(filepaths) {
         return new Promise((resolve) => {
-            const loadPromises = filepaths.map((filepath) => {
-                return Loader.loadFile(filepath);
+            const loadPromises = Promise.map(filepaths, (filepath) => {
+                return Loader.loadFile(filepath).reflect();
+            }, { concurrency: Loader.MAX_CONCURRENCY });
+            resolve(loadPromises);
+        }).then((loadResults) => {
+            return loadResults.map((loadResult) => {
+                return loadResult.isFulfilled() ? loadResult.value() : loadResult.reason();
             });
-            resolve(Promise.all(loadPromises));
         });
     }
 
     static loadFile(filepath) {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             fs.readFile(filepath, 'utf-8', (err, data) => {
                 if (err) {
-                    resolve(err);
+                    reject(err);
                 } else {
                     resolve(data);
                 }
@@ -49,5 +91,7 @@ Loader.LOADER_METHODS = {
     '.yml': Loader.loadYAML,
     '.json': Loader.loadJSON,
 };
+
+Loader.MAX_CONCURRENCY = 500;
 
 module.exports = Loader;
--- tests/loader-test.js
+++ tests/loader-test.js
@@ -3,42 +3,94 @@ const fs = require('fs');
 const jsYaml = require('js-yaml');
 
 const Loader = require('../src/loader');
-const packageJson = require('../package.json');
 
 describe('Loader', () => {
-    it('loads arrays of files', (done) => {
-        Loader.loadFiles(
-            [`${__dirname}/../package.json`, `${__dirname}/../package.json`]
-        ).then((files) => {
-            assert.lengthOf(files, 2);
-            assert.isAtLeast(files[0].length, 100);
-            done();
-        }).catch(done);
-    });
+    describe('path resolving', () => {
+        it('resolves multiple string paths', (done) => {
+            const inputPaths = [`${__dirname}/../package.json`, `${__dirname}/../README.md`];
+            Loader.resolvePaths(inputPaths).then((filepaths) => {
+                assert.deepEqual(filepaths, inputPaths);
+                done();
+            }).catch(done);
+        });
 
-    it('returns errors for non-existent files', (done) => {
-        Loader.loadFiles(['dne.js']).then((files) => {
-            assert.typeOf(files[0], 'Error');
-            done();
-        }).catch(done);
-    });
+        it('resolves mixtures of glob and string paths', (done) => {
+            const inputPaths = [`${__dirname}/*.js`, `${__dirname}/../README.md`];
+            Loader.resolvePaths(inputPaths).then((filepaths) => {
+                assert.isTrue(filepaths.length > 1);
+                filepaths.forEach((filepath) => { assert.isString(filepath); });
+                done();
+            }).catch(done);
+        });
 
-    it('loads yaml data', () => {
-        const yamlPath = `${__dirname}/fixtures/project/swaggerBase.yaml`;
-        const yamlObject = jsYaml.load(fs.readFileSync(yamlPath, 'utf-8'));
-        Loader.loadData(yamlPath).then((yaml) => {
-            Object.keys(yamlObject).forEach((key) => {
-                assert.isDefined(yaml[key]);
+        it('can resolve a string path', (done) => {
+            Loader.resolvePaths(`${__dirname}/*.js`).then((filepaths) => {
+                assert.isTrue(filepaths.length > 1);
+                filepaths.forEach((filepath) => { assert.isString(filepath); });
+                done();
+            }).catch(done);
+        });
+
+        it('resolves a string glob', (done) => {
+            Loader.resolvePath(`${__dirname}/../*.json`).then((filepaths) => {
+                assert.isTrue(filepaths.length >= 1);
+                assert.include(filepaths[0], 'package.json');
+                done();
+            }).catch(done);
+        });
+
+        it('resolves a string path', (done) => {
+            Loader.resolvePath(__filename).then((filepaths) => {
+                assert.lengthOf(filepaths, 1);
+                done();
+            }).catch(done);
+        });
+
+        it('rejects if the path is invalid', (done) => {
+            Loader.resolvePath(['./nonsense']).then(() => {
+                done(new Error('Loader.resolvePath was expected to reject'));
+            }).catch((err) => {
+                assert.include(err.toString(), 'TypeError');
+                done();
             });
         });
     });
 
-    it('loads json data', () => {
-        const jsonPath = `${__dirname}/fixtures/project/swaggerBase.json`;
-        const jsonObject = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'));
-        Loader.loadData(jsonPath).then((json) => {
-            Object.keys(jsonObject).forEach((key) => {
-                assert.isDefined(json[key]);
+    describe('file loading', () => {
+        it('loads arrays of files', (done) => {
+            Loader.loadFiles(
+                [`${__dirname}/../package.json`, `${__dirname}/../package.json`]
+            ).then((files) => {
+                assert.lengthOf(files, 2);
+                assert.isAtLeast(files[0].length, 100);
+                done();
+            }).catch(done);
+        });
+
+        it('returns errors for non-existent files', (done) => {
+            Loader.loadFiles(['dne.js']).then((files) => {
+                assert.typeOf(files[0], 'Error');
+                done();
+            }).catch(done);
+        });
+
+        it('loads yaml data', () => {
+            const yamlPath = `${__dirname}/fixtures/project/swaggerBase.yaml`;
+            const yamlObject = jsYaml.load(fs.readFileSync(yamlPath, 'utf-8'));
+            Loader.loadData(yamlPath).then((yaml) => {
+                Object.keys(yamlObject).forEach((key) => {
+                    assert.isDefined(yaml[key]);
+                });
+            });
+        });
+
+        it('loads json data', () => {
+            const jsonPath = `${__dirname}/fixtures/project/swaggerBase.json`;
+            const jsonObject = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'));
+            Loader.loadData(jsonPath).then((json) => {
+                Object.keys(jsonObject).forEach((key) => {
+                    assert.isDefined(json[key]);
+                });
             });
         });
     });

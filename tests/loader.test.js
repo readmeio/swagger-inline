d8d281bf43c5569df22e10c899e589c5d7ab573a
Jon Ursenbach <jon@ursenba.ch>
2019-12-08 16:12:21 -0800

    test: moving all tests over to jest

--- tests/loader.test.js
+++ tests/loader.test.js
@@ -1,4 +1,3 @@
-const assert = require('chai').assert;
 const fs = require('fs');
 const jsYaml = require('js-yaml');
 
@@ -6,95 +5,81 @@ const Loader = require('../src/loader');
 
 describe('Loader', () => {
   describe('.resolvePaths', () => {
-    it('resolves multiple string paths', done => {
+    it('resolves multiple string paths', () => {
       const inputPaths = [`${process.cwd()}/package.json`, `${process.cwd()}/README.md`];
-      Loader.resolvePaths(inputPaths)
-        .then(filepaths => {
-          assert.deepEqual(filepaths, inputPaths);
-          done();
-        })
-        .catch(done);
+      return Loader.resolvePaths(inputPaths).then(filepaths => {
+        expect(filepaths).toStrictEqual(inputPaths);
+      });
     });
 
-    it('resolves mixtures of glob and string paths', done => {
+    it('resolves mixtures of glob and string paths', () => {
       const inputPaths = [`${process.cwd()}/tests/*.js`, `${process.cwd()}/README.md`];
-      Loader.resolvePaths(inputPaths)
-        .then(filepaths => {
-          assert.isTrue(filepaths.length > 1);
-          filepaths.forEach(filepath => {
-            assert.isString(filepath);
-          });
-          done();
-        })
-        .catch(done);
+      return Loader.resolvePaths(inputPaths).then(filepaths => {
+        expect(filepaths.length).toBeGreaterThan(1);
+        filepaths.forEach(filepath => {
+          expect(typeof filepath).toBe('string');
+        });
+      });
     });
 
-    it('can resolve a string path', done => {
-      Loader.resolvePaths(`${__dirname}/*.js`)
-        .then(filepaths => {
-          assert.isTrue(filepaths.length > 1);
-          filepaths.forEach(filepath => {
-            assert.isString(filepath);
-          });
-          done();
-        })
-        .catch(done);
+    it('can resolve a string path', () => {
+      return Loader.resolvePaths(`${__dirname}/*.js`).then(filepaths => {
+        expect(filepaths.length).toBeGreaterThan(1);
+        filepaths.forEach(filepath => {
+          expect(typeof filepath).toBe('string');
+        });
+      });
     });
   });
 
   describe('.loadBase', () => {
     it('loads an empty object if no swagger is found', () => {
       return Loader.loadBase().then(base => {
-        assert.deepEqual(base, {});
+        expect(base).toStrictEqual({});
       });
     });
 
     it('loads json file data', () => {
       const jsonPath = `${__dirname}/fixtures/project/swaggerBase.json`;
       return Loader.loadBase(jsonPath).then(base => {
-        assert.isDefined(base.swagger);
+        expect(base.swagger).not.toBeUndefined();
       });
     });
 
     it('loads yaml file data', () => {
       const yamlPath = `${__dirname}/fixtures/project/swaggerBase.yaml`;
       return Loader.loadBase(yamlPath).then(base => {
-        assert.isDefined(base.swagger);
+        expect(base.swagger).not.toBeUndefined();
       });
     });
 
     it('searches for swagger in the provided directory', () => {
       const dir = `${__dirname}/fixtures/project/`;
       return Loader.loadBase(dir).then(base => {
-        assert.isDefined(base.swagger);
+        expect(base.swagger).not.toBeUndefined();
       });
     });
 
     it('returns an empty object if swagger is not found in a directory', () => {
       return Loader.loadBase(__dirname).then(base => {
-        assert.deepEqual(base, {});
+        expect(base).toStrictEqual({});
       });
     });
   });
 
   describe('.loadFiles', () => {
-    it('loads arrays of files', done => {
-      Loader.loadFiles([`${__dirname}/../package.json`, `${__dirname}/../package.json`])
-        .then(files => {
-          assert.lengthOf(files, 2);
-          assert.isAtLeast(files[0].length, 100);
-          done();
-        })
-        .catch(done);
+    it('loads arrays of files', () => {
+      return Loader.loadFiles([`${__dirname}/../package.json`, `${__dirname}/../package.json`]).then(files => {
+        expect(files).toHaveLength(2);
+        expect(files[0].length).toBeGreaterThan(100);
+      });
     });
 
-    it('returns errors for non-existent files', done => {
-      Loader.loadFiles(['dne.js'])
-        .then(files => {
-          assert.typeOf(files[0], 'Error');
-          done();
-        })
-        .catch(done);
+    it('returns errors for non-existent files', () => {
+      return Loader.loadFiles(['dne.js']).then(files => {
+        expect(typeof files[0]).toBe('object');
+        expect(files[0].message).toContain('no such file or directory');
+      });
     });
   });
 
@@ -158,7 +143,7 @@ describe('Loader', () => {
       };
 
       Object.keys(test).forEach(k => {
-        assert.deepEqual(Loader.expandParam(k), test[k]);
+        expect(Loader.expandParam(k)).toStrictEqual(test[k]);
       });
     });
   });
@@ -245,7 +230,7 @@ describe('Loader', () => {
       };
 
       Object.keys(test).forEach(k => {
-        assert.deepEqual(Loader.expandParam(k, 3), test[k]);
+        expect(Loader.expandParam(k, 3)).toStrictEqual(test[k]);
       });
     });
   });
@@ -254,9 +239,9 @@ describe('Loader', () => {
     it('loads yaml data', () => {
       const yamlPath = `${__dirname}/fixtures/project/swaggerBase.yaml`;
       const yamlObject = jsYaml.load(fs.readFileSync(yamlPath, 'utf-8'));
-      Loader.loadData(yamlPath).then(yaml => {
+      return Loader.loadData(yamlPath).then(yaml => {
         Object.keys(yamlObject).forEach(key => {
-          assert.isDefined(yaml[key]);
+          expect(yaml[key]).not.toBeUndefined();
         });
       });
     });
@@ -264,9 +249,9 @@ describe('Loader', () => {
     it('loads json data', () => {
       const jsonPath = `${__dirname}/fixtures/project/swaggerBase.json`;
       const jsonObject = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'));
-      Loader.loadData(jsonPath).then(json => {
+      return Loader.loadData(jsonPath).then(json => {
         Object.keys(jsonObject).forEach(key => {
-          assert.isDefined(json[key]);
+          expect(json[key]).not.toBeUndefined();
         });
       });
     });

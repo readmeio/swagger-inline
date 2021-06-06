263ca6cfe4819b49bb565bfae7b768de3d8e9d5a
Jon Ursenbach <jon@ursenba.ch>
2021-06-06 12:21:09 -0700

    fix: ignore errors where a files language can't be determined

--- src/index.js
+++ src/index.js
@@ -92,6 +92,8 @@ function swaggerInline(globPatterns, providedOptions) {
                 // If the file that we failed to parse is a text file, let's just ignore it.
                 if (['.json', '.md', '.txt'].includes(path.extname(fileInfo.fileName))) {
                   return Promise.resolve();
+                } else if (new RegExp(/Cannot find language definition/).test(err.message)) {
+                  return Promise.resolve();
                 }
 
                 return Promise.reject(new Error(`${err.toString()} \n at ${fileInfo.fileName}`));

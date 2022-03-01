e8e3e21d550b04457e3616a374ec4540ff7a9eb9
Garrett Wade <42807642+garrett-wade@users.noreply.github.com>
2022-03-01 15:44:54 -0700

    Update src/cli.js
    Co-authored-by: Jon Ursenbach <erunion@users.noreply.github.com>

--- src/cli.js
+++ src/cli.js
@@ -10,7 +10,7 @@ module.exports = function (args) {
     .option('-b, --base [path]', 'A base OpenAPI or Swagger definition to build off of.')
     .option('-f, --format [format]', 'Output format (.json or .yaml).')
     .option('-s, --scope [scope]', 'API scope to constrain against')
-    .option('-p, -- pattern [pattern]', 'Custom comment pattern supported by the multilang-extract-comments library in the form of a JSON file');
+    .option('-p, --pattern [pattern]', 'Custom comment pattern supported by the multilang-extract-comments library in the form of a JSON file');
 
   program.on('--help', () => {
     console.log('');
